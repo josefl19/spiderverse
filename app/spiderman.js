1c6187d6087555869b8d07b0355440b851be8bc1
josefl <jose.fl_@hotmail.com>
2022-04-27 22:24:58 -0500

    spiderman class and test

---
+++ app/spiderman.js
@@ -0,0 +1,11 @@
+class Spiderman {
+    constructor(nombre, edad, actor, noPeliculas, estudio) {
+        this.nombre = nombre;
+        this.edad = edad;
+        this.actor = actor;
+        this.noPeliculas = noPeliculas;
+        this.estudio = estudio;
+    }
+}
+
+module.exports = Spiderman;
--- test/spiderman.test.js
+++ test/spiderman.test.js
@@ -1,6 +1,13 @@
-describe("Test Suite Dummy Description", () => {
-    test('Case 1 Dummy', () => {
-      const resultOfSomething = 1 + 2
-      expect(resultOfSomething).toBe(18);
+const Spiderman = require('../app/spiderman');
+
+describe("Unit tests for Spiderman class", () => {
+    test('1) Create an spiderman object', () => {
+        const tomHolland = new Spiderman("Spider-Man: No Way Home", 25, "Tom Holland", 24, "Sony");
+        
+        expect(tomHolland.nombre).toBe("Spider-Man: No Way Home");
+        expect(tomHolland.edad).toBe(25);
+        expect(tomHolland.actor).toBe("Tom Holland");
+        expect(tomHolland.noPeliculas).toBe(24);
+        expect(tomHolland.estudio).toBe("Sony");
     });
 })
